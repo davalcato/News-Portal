830013db7c05e66dac5fd9a1f1718cf88d530b1f
davalcato <davalcato@yahoo.com>
2020-05-18 20:50:59 -0700

    first commit

---
+++ News Portal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News Portal
+//
+//  Created by Daval Cato on 5/18/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
