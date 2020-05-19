56b29efc3af47dc4156cc12cceffea221aa95001
davalcato <davalcato@yahoo.com>
2020-05-18 21:16:42 -0700

    added the let url = URL (string: source) and  let session = URLSession to the ContentView file

--- News Portal/ContentView.swift
+++ News Portal/ContentView.swift
@@ -36,7 +36,33 @@ class getDate : ObservedObject{
     
     @Published var datas = [dataType]()
     
-    
+    init() {
+        
+        let source = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-19&sortBy=publishedAt&apiKey=60f8409fa20b4a5ea3a54ee3d1f229e9"
+        
+        let url = URL (string: source)!
+        
+        let session = URLSession(configuration: .default)
+        
+        session.dataTask(with: url) { (data, _, err) in
+            
+            if Err != nil{
+                
+                print((err?.localizedDescription)!)
+                return
+                
+            }
+            
+            let json = try! JSON(data: data!)
+            
+            for i in json["articles"]{
+                
+                let title =
+                
+                
+            }
+        }
+    }
 }
 
 // This is where we generate an API in News API.
